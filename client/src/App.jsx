a5099236edd3a15dab5613831c02203cb8c78ee2
Gabriele Scamardo <scamardo.website@gmail.com>
2024-06-15 10:22:16 +0200

    boostrap installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {useSwipeable} from 'react-swipeable'
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const handlers = useSwipeable({
-    onSwipedLeft: () => setIsOpen(false),
-    onSwipedRight: () => setIsOpen(true),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <div {...handlers} className={`drawer ${isOpen ? 'open' : ''}`}>
-      <div className="drawer-content">
-        <h2>Drawer Content</h2>
-        <p>This is the content inside the drawer.</p>
-      </div>
-      <button onClick={() => setIsOpen(!isOpen)} className="toggle-button">
-        {isOpen ? 'Close' : 'Open'}
-      </button>
-    </div>
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Button variant="primary" onClick={handleShow}>
+              <i className="bi bi-list-nested"></i>
+            </Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
